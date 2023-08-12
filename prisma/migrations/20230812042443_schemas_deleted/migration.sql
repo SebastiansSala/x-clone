43451a5d2b5ad9e46f3637947b50774a0688d527
Sebastian Sala <sebastian_orko@hotmail.com>
2023-08-11 22:39:30 -0600

    clerk added, working on it tho

---
+++ prisma/migrations/20230812042443_schemas_deleted/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "messages" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "image" TEXT,
+    "senderId" TEXT NOT NULL,
+    "recipientId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_recipientId_fkey" FOREIGN KEY ("recipientId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
