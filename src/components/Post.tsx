9cd1e35a307b15a485e561f8532154567d471bd8
Sebastian Sala <sebastian_orko@hotmail.com>
2023-08-18 19:26:35 -0600

    author call removed

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,7 +1,5 @@
 import { Avatar } from "@nextui-org/avatar"
-import React from "react"
-import prisma from "@/utils/prisma"
-import { type PostWithAll, type Post } from "@/types/db"
+import { type Post } from "@/types/db"
 import {
   CommentIcon,
   LikeIcon,
@@ -15,22 +13,14 @@ type PostProps = {
 }
 
 const Post = async ({ post }: PostProps) => {
-  if (!post.authorId) return
-
-  const author = await prisma.users.findFirst({
-    where: {
-      id: post.authorId,
-    },
-  })
-
   return (
     <li className='grid grid-cols-12 border-t-1 border-[#2f3336] p-4'>
-      <Avatar className='col-span-1' src={author?.avatar_url} />
+      <Avatar className='col-span-1' src={post.author?.avatar_url} />
       <div className='col-span-11'>
         <div className='flex justify-between items-center'>
           <div className='flex gap-4'>
-            <h4>{author?.name}</h4>
-            <p className='text-gray-500'>{author?.user_name}</p>
+            <h4>{post?.author.avatar_url}</h4>
+            <p className='text-gray-500'>{post.author?.user_name}</p>
           </div>
           <Button
             radius='full'
